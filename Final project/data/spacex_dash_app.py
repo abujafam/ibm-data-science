b8685d59f0439505eee2ab66852767bdb5627d68
abujafam <115104638+abujafam@users.noreply.github.com>
2023-02-13 16:58:42 +0000

    adding photos

--- Final project/data/spacex_dash_app.py
+++ Final project/data/spacex_dash_app.py
@@ -7,7 +7,7 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 
 # Read the airline data into pandas dataframe
-spacex_df = pd.read_csv("D:\IBM DATA SCIENCE\finale project\ibm-data-science\Final project\data\spacex_launch_dash.csv")
+spacex_df = pd.read_csv("data\spacex_launch_dash.csv")
 max_payload = spacex_df['Payload Mass (kg)'].max()
 min_payload = spacex_df['Payload Mass (kg)'].min()
 
